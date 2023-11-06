3885cbfe0e64812967c92f8ff13212c7780d9b8e
한지원 <88698607+jiwon83@users.noreply.github.com>
2023-11-06 23:13:51 +0900

    [231106] feat : solve BOJ 13023 ABCDE (#111)
    Co-authored-by: jiwons0803@naver.com <jiwons0803@naver.com>

---
+++ jiwon83/BOJ_13023_ABCDE.java
@@ -0,0 +1,74 @@
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.CookieHandler;
+import java.util.*;
+
+public class Main {
+
+    static ArrayList<Integer>[] graph;
+    static int [] childs;
+    static ArrayList<Integer> roots;
+    static boolean [] visit;
+    static int N, M;
+    static boolean flag=false;
+    static BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        //1. 그래프를 그린다.
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        childs = new int[N];
+        visit = new boolean[N];
+        graph = new ArrayList[N];
+        roots = new ArrayList<>();
+
+        for (int i=0; i<N; i++) graph[i] = new ArrayList<>();
+
+        //2. root들로 탐색한다.
+        for (int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            graph[from].add(to);
+            graph[to].add(from);
+            childs[from]++;
+            childs[to]++;
+        }
+        for (int i=0; i<N; i++){
+            if (childs[i]==1) roots.add(i);
+        }
+
+        for (int r=0; r < N; r++){
+            if (visit[r]) continue;
+            visit[r]=true;
+            dfs(1,r);
+            visit[r]=false;
+        }
+
+
+        if (flag) System.out.println(1);
+        else System.out.println(0);
+
+    }
+
+    static void dfs(int depth, int x){
+        if (flag) return;
+        if (depth == 5){
+            flag = true;
+        } else {
+            for (int y : graph[x]){
+                if(visit[y]) continue;
+                visit[y] = true;
+                dfs(depth+1, y);
+                visit[y] = false;
+
+            }
+        }
+
+    }
+}
