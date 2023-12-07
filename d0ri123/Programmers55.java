14d357aa45e70cfc7074a46abb20160c814928c8
Seolhui Lee <94590894+D0ri123@users.noreply.github.com>
2023-12-07 16:43:56 +0900

    [231206] feat: solve 프로그래머스 - 햄버거 만들기 (#248)

---
+++ d0ri123/Programmers55.java
@@ -0,0 +1,27 @@
+package d0ri123;
+
+import java.util.Stack;
+
+public class Programmers55 {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for(int num : ingredient) {
+            stack.push(num);
+
+            if(stack.size() >= 4) {
+                if(stack.get(stack.size() - 4) == 1
+                    && stack.get(stack.size() - 3) == 2
+                    && stack.get(stack.size() - 2) == 3
+                    && stack.get(stack.size() - 1) == 1) {
+                    answer++;
+
+                    for(int i=1; i<=4; i++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
