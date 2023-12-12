7c77d2e098b2320b91e038cf31b7923ed1d95a17
ssheon0812 <83093525+suheoon@users.noreply.github.com>
2023-12-12 23:41:35 +0900

    [231212] feat: solve boj 카드게임 (#276)

---
+++ suheoon/boj_5522.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int sum = 0;
+    for (int i = 0; i < 5; i++) {
+        int num;
+        cin >> num;
+        sum += num;
+    }
+
+    cout << sum;
+    
+    return 0;
+}
