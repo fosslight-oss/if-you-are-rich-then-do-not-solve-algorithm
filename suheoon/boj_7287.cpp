00655bf6248e2f9b1e9068d53830aa1b730912a0
suheoon <ssheon0812@naver.com>
2023-12-13 22:18:51 +0900

    feat: solve boj

---
+++ suheoon/boj_7287.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cout << 658 << '\n';
+    cout << "songsuheon97" << '\n';
+
+    return 0;
+}
