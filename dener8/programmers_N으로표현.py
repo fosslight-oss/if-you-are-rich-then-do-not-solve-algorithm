063510f689dadec378f680344206c1619b8e6348
dener8 <8dener@gmail.com>
2023-12-18 03:55:11 +0900

    [231217] feat: solve programmers N으로표현

---
+++ dener8/programmers_N으로표현.py
@@ -0,0 +1,23 @@
+'''
+return: 1 ~ 8 혹은 -1 (9 이상)
+'''
+
+def solution(N, number):
+    dp = []
+
+    for cnt in range(1, 9):  # 1 ~ 8 모두 확인하기
+        cnt_set = set()  # 해당 cnt 개수로 만들 수 있는 숫자 집합
+        cnt_set.add(int(str(N) * cnt))
+        for i in range(0, cnt - 1):  # dp[] 길이만큼
+            for x in dp[i]:
+                for y in dp[-i - 1]:
+                    cnt_set.add(x + y)
+                    cnt_set.add(abs(x - y))
+                    cnt_set.add(x * y)
+                    if y != 0:
+                        cnt_set.add(x // y)
+        if number in cnt_set:
+            return cnt
+        dp.append(cnt_set)
+
+    return -1
