d53fef3d13b51a090ecea5941caa4cbf0d595299
Jiwon Han <gkswldnj0803@gmail.com>
2024-01-07 21:46:21 +0900

    [240107] feat: solve boj 2504 괄호의값

---
+++ jiwon83/BOJ_2504_괄호의값.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ 4 5
+ 50 45 37 32 30
+ 35 50 40 20 25
+ 30 30 25 17 28
+ 27 24 22 15 10
+ */
+class Main {
+   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   static StringTokenizer st;
+   static int answer;
+   static String str;
+   static void input() throws IOException{
+      str = br.readLine();
+   }
+   /*
+   (1+(2*(3+4)))
+   (1+(2*3+4))
+   (1+2*(3+4))
+   (1+2*3+4)
+   1+(2*(3+4))
+    */
+   static boolean sol(String str){
+      //괄호의 값을 더이상 분해할 수 없을 때까지 분해하고 값을 return
+      ArrayDeque<Object> stack = new ArrayDeque<>();
+      answer = 0;
+      for (int i = 0; i<str.length(); i++){
+         char c = str.charAt(i);
+         if (c == '(' || c == '['){
+            stack.addLast(c);
+         }else if(c == ')'){
+            int num = 0;
+            if(!stack.isEmpty() && stack.peekLast() instanceof Character && (char) stack.peekLast() == '(') num = 2;
+            else{
+               while (!stack.isEmpty() && ( stack.peekLast() instanceof Integer || (char)stack.peekLast() != '(')){
+                  Object out = stack.pollLast();
+                  if (out instanceof Integer) num += (int) out;
+                  else if((char)out == '[') return false;
+               }
+               num *= 2;
+            }
+
+            if (stack.isEmpty()) return false;
+            stack.pollLast();
+            stack.addLast(num);
+
+         }else if(c == ']'){
+            int num = 0;
+            if(!stack.isEmpty() && stack.peekLast() instanceof Character && (char) stack.peekLast() == '[') num = 3;
+            else{
+               while (!stack.isEmpty() && ( stack.peekLast() instanceof Integer || (char)stack.peekLast() != '[')){
+                  Object out = stack.pollLast();
+                  if(out instanceof Integer) num += (int) out;
+                  else if((char)out == '(') return false;
+               }
+               num *= 3;
+            }
+
+            if (stack.isEmpty()) return false;
+            stack.pollLast();
+            stack.addLast(num);
+
+         }
+      }
+      while (!stack.isEmpty()){
+         Object obj = stack.pollLast();
+         if(obj instanceof Character) return false;
+         answer += (int) obj;
+      }
+      return true;
+   }
+
+   public static void main(String[] args) throws IOException {
+      input();
+      if(sol(str)){
+         System.out.println(answer);
+      }else{
+         System.out.println(0);
+      }
+
+   }
+}
