db9286d91624bb8319378868c295a04d05ef9268
dener8 <84485400+dener8@users.noreply.github.com>
2024-01-19 04:40:06 +0900

    [240118] feat: solve programmers 추억점수 (#360)

---
+++ dener8/programmers_추억점수.py
@@ -0,0 +1,13 @@
+def solution(name, yearning, photos):
+    answer = []
+    dictionary = dict()
+    for i in range(len(name)):
+        dictionary[name[i]] = yearning[i]
+    for photo in photos:
+        score = 0
+        for person in photo:
+            if person in dictionary:
+                score += dictionary[person]
+        answer.append(score)
+
+    return answer₩
